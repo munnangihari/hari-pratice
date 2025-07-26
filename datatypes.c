46b96468753149444bbd24764928dceff2ad2ba3
munnangihari <harivardhanmunnangi2003@gmail.com>
2025-07-26 09:41:17 +0530

    datatypes

---
+++ datatypes.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <stdbool.h>
+int main()
+{
+	int a=20;
+	float b=5.6;
+	double c=45.2;
+	char d='z';
+	bool e=0;
+	printf("%d\n%f\n%lf\n%c\n%hhd\n",a,b,c,d,e);
+	return 0;
+}
+
