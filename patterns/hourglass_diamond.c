cc4468ef9e442fc2d66042ba6a8def017454e467
munnangihari <harivardhanmunnangi2003@gmail.com>
2025-08-29 14:27:28 +0530

    hourglass_diamond

---
+++ patterns/hourglass_diamond.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	int i, j;
+	printf("enter n:\n");
+       scanf("%d", &n);
+	for (i = 0; i <= n; i++)
+	{
+		for(j = 0; j <= n - i; j++)
+		{
+			printf("* ");
+		}
+		for(j = 0; j < 2 * i; j++)
+		{
+			printf("  ");
+		}
+		for(j = 0; j <= n - i; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	for(i = n; i >= 0; i--)
+	{
+		for (j = 0; j <= n - i; j++)
+		{
+			printf("* ");
+		}
+		for(j = 0; j < 2 * i; j++)
+		{
+			printf("  ");
+		}
+		for(j = 0; j <= n - i; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0; 
+}
+
