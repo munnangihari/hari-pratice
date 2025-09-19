5908652a4bef954f1df7c02d67fad9dcaf203f64
munnangihari <harivardhanmunnangi2003@gmail.com>
2025-09-19 16:21:46 +0530

    duplicates

--- arrays/duplicates.c
+++ arrays/duplicates.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
 int main()
 {
-	int n;
+	int n, m =0;
 	printf("enter the size:\n");
 	scanf("%d", &n);
-	int a[n];
+	int a[n], d[n];
 	printf("enter array:\n");
 	for(int i = 0; i < n; i++)
 	{
 		scanf("%d", &a[i]);
 	}
-	int count = 0;
 	for(int i = 0; i < n; i++)
 	{
+		int count = 0;
 		for(int j = i + 1; j < n; j++)
 		{
 			if(a[i] == a[j])
 			{
-				printf("%d", a[i]);
-				count++;
-				break;
+				for(int k = 0; k < n; k++)
+				{
+					if(a[i] == d[k])
+					{
+						count++;
+					}
+				}
+				if(count == 0)
+				{
+					d[m] = a[i];
+					m++;
+				}
 			}
 		}
 	}
-	if(count == 0)
+	for(int i = 0; i < m; i++)
 	{
-		printf("NONE\n");
+		printf("%d ", d[i]);
 	}
 	return 0;
 }
-
