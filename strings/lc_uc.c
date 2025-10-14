8d66f69a3601498bc118b28b8cf78866be54cf9d
munnangihari <harivardhanmunnangi2003@gmail.com>
2025-10-14 12:37:32 +0530

    lc_uc.c

---
+++ strings/lc_uc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+	char str[10];
+	int i;
+	printf("enter string\n");
+	scanf("%s",str);
+	for(i = 0;str[i]!='\0';i++)
+	{
+		/*if(str[i]>= 65 && str[i]<=90)
+		{
+			str[i] = str[i] + 32;
+		}
+		if(str[i]>= 97 && str[i]<=122)
+                {       
+                        str[i] = str[i] - 32;
+                }*/
+	       if(str[i]>= 'a' && str[i]<= 'z')
+                {
+                        str[i] = str[i] - 32;
+                }
+	       if(str[i]>= 'A' && str[i]<='Z')
+                {
+                        str[i] = str[i] + 32;
+                }
+
+
+	}
+	printf("%s",str);
+	return 0;
+}
