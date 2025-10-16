c663df781c9684c155c2e11fccd9ae9aa8ba62f6
munnangihari <harivardhanmunnangi2003@gmail.com>
2025-10-16 11:32:30 +0530

    decending

---
+++ test/decending.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int arr[5] = {5,7,3,9,1};
+	for(int i = 0; i <= 4; i++)
+	{
+		for(int j = i+1; j < 5; j++)
+		{
+			if(arr[i] < arr[j])
+			{
+				int temp = arr[i];
+				arr[i] = arr[j]; 
+				arr[j] = temp;
+			}
+		}
+	}
+	for(int i = 0; i <= 4; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	return 0;
+}
